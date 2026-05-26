561cb2ace814d98ddcbe865a4fe9c0e9f53a0a3c
Wesley Bezerra <wesleybezerra770@gmail.com>
2026-05-26 17:48:12 -0300

    .

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
     return (
         <>
         <h1>
-            Login
+            <Link to="/Home" style={{ textDecoration: 'none', color: 'white' }}>
+                Home
+            </Link>
         </h1>
         </>
     )
